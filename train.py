bbf86dcb89def0219f07f1cc79a1bfb9d61a30e4
xrachitx <45661647+xrachitx@users.noreply.github.com>
2023-11-10 15:17:45 +0530

    Update train.py

--- train.py
+++ train.py
@@ -45,7 +45,7 @@ parser.add_argument('--dice_flag', dest='dice_flag', action='store_true',
                     help='Using Dice loss')
 parser.add_argument('--double_channel', dest='double_channel', action='store_true',
                     help='Using 2 channel')
-parser.add_argument('--learn_from_pretrained', dest='is_pretrain', action='store_true',
+parser.add_argument('--learn_from_pretrained', dest='learn_from_pretrained', action='store_true',
                     help='Using pretrained model')
 parser.add_argument('--is_pretrain', dest='is_pretrain', action='store_true',
                     help='Continue training again')
