00bcf08c4353b2f4028df4ae788d5ee77341f6dd
xrachitx <45661647+xrachitx@users.noreply.github.com>
2024-06-22 19:07:47 +0530

    added code for dilation

--- dataloader.py
+++ dataloader.py
@@ -4,17 +4,19 @@ import pandas as pd
 from torchvision import transforms
 import skimage.io as io
 import skimage
+from skimage.morphology import skeletonize, dilation, diamond
 from torch.utils.data import Dataset, DataLoader
 import torch
 import cv2
 
 
 class LoadData(Dataset):
-    def __init__(self, fileNames, rootDir, double_channel = False,transform=None):
+    def __init__(self, fileNames, rootDir, dilate_skel, double_channel = False,transform=None):
         self.rootDir = rootDir
         self.transform = transform
         self.dc = double_channel
         self.frame = pd.read_csv(fileNames, dtype=str, delimiter=',', header=None)
+        self.dilate_skel = dilate_skel
     
     def __len__(self):
         return len(self.frame)
@@ -23,40 +25,27 @@ class LoadData(Dataset):
 
         inputName = os.path.join(self.rootDir, self.frame.iloc[idx, 0][1:])
         targetName = os.path.join(self.rootDir, self.frame.iloc[idx, 1][1:])
-        # print(inputName,targetName,self.rootDir, self.frame.iloc[idx, 1][1:])
         inputImage = cv2.imread(inputName)
         targetImage = cv2.imread(targetName, cv2.IMREAD_GRAYSCALE)
         
         targetImage = targetImage > 0.0
-        if self.dc:
-            out_im = np.zeros((448,448,2))
-            out_im[:,:,0] = np.where(targetImage == 0, 1, 0)
-            out_im[:,:,1] = np.where(targetImage == 1, 1, 0)
-            out_im = out_im.astype(np.float32)
-            out_im = out_im.transpose((2, 0, 1))
+        
+        if self.dilate_skel:
+            dilated_target = skeletonize(targetImage)
+            dilated_target = dilation(targetImage, diamond(2))
+            dilated_target = second_target_img.astype(np.float32)
+            dilated_target = np.expand_dims(second_target_img,axis=0)
+                
         counts = np.unique(targetImage,return_counts=True)[1]
         weights = np.array([ counts[0]/(counts[0]+counts[1]) , counts[1]/(counts[0]+counts[1]) ])
         inputImage = inputImage.astype(np.float32)
+
         targetImage = targetImage.astype(np.float32)
         inputImage = inputImage.transpose((2, 0, 1))
+
         targetImage = np.expand_dims(targetImage,axis=0)
         
-#         return inputImage, targetImage,weights
-#         if not self.dc:
-        return inputImage, targetImage,weights, self.frame.iloc[idx, 0]
-#         else:
-#             print("IT R DOUBLE CHANNEL")
-#             return inputImage, out_im,weights, self.frame.iloc[idx, 0]
-        
-
-if __name__ == "__main__":
-    rootDir ="./CoSkel+"
-    files = "./CoSkel+/train.csv"
-
-    td = LoadData(files, rootDir)
-    train_dataloader = DataLoader(td,batch_size=20)
-    # print(train_dataloader)
-    for i, (data) in enumerate(train_dataloader,0):
-        print(data[0].shape,data[1].shape,data[2])
-        exit()
-    # print(len(train_dataloader))
+        if self.dilate_skel:
+            return inputImage, targetImage,weights, dilated_target, self.frame.iloc[idx, 0]
+        else:
+            return inputImage, targetImage,weights, self.frame.iloc[idx, 0]
